5cec1ca3e1903222a871baa7ad75e04fbb9eb382
italo.guglielmini <italo.guglielmini@proluno.com.br>
2023-10-12 16:30:58 -0300

    feat: ajustes da formatação do output do chatGPT e display inside RN

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -9,16 +9,49 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [outputMessage, setoutputMessage] = useState(null);
+
+  const handleButtonClick = () => {
+    console.log("Button Clicado", inputMessage);
+    fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer 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", // trocar para sua key
+      },
+      body: JSON.stringify({
+        prompt: inputMessage,
+        model: "text-davinci-003",
+      }),
+    })
+      .then((responce) => responce.json())
+      .then((data) => {
+        console.log("data", data.choices[0].text);
+        setoutputMessage(data.choices[0].text.trim());
+      });
+  };
+
+  const handleTextInput = (text: string) => {
+    setInputMessage(text);
+    console.log(text);
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>Results chats aqui</Text>
+        <Text>{outputMessage}</Text>
       </View>
       <View style={styles.content}>
         <View style={{ flex: 1 }}>
-          <TextInput style={styles.input} placeholder="Pergunte me" />
+          <TextInput
+            style={styles.input}
+            placeholder="Pergunte me"
+            onChangeText={handleTextInput}
+          />
         </View>
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
           <Text style={{ color: "white", textAlign: "center" }}>Enviar</Text>
         </TouchableOpacity>
       </View>
@@ -36,8 +69,8 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 50,
+    padding: 16,
     backgroundColor: "#e1e1e1",
-    paddingHorizontal: 8,
   },
   content: {
     width: "100%",
